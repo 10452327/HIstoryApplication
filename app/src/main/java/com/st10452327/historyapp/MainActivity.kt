ce6d6ea295da40c35f7e9cda06c3483b4a1bb9f9
lab_services_student <sphes495@gmail.com>
2024-04-03 13:12:04 +0200

    My First App

--- app/src/main/java/com/st10452327/historyapp/MainActivity.kt
+++ app/src/main/java/com/st10452327/historyapp/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.st10452327.historyapp
+//ST10452327 Siphesihle Siphamandla Bandile Nkabinde
 
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
@@ -34,26 +35,26 @@ class MainActivity : AppCompatActivity() {
             if (age != null && age in 20..100){
                 // when age is 30 display Thato else when is .. and so on
                 val Historicalfigure = when(age){
-                    81 -> "Winnie Madikizela-Mandela"
-                    44 -> "David Webster"
-                    91 -> "Helen Suzman"
-                    22 -> "Solomon Mahlangu"
-                    30 -> "Steve Biko"
-                    92 -> "Albertina Sisulu"
-                    69 -> "Lilian Masediba Ngoyi"
-                    87 -> "Helen joseph"
-                    68 -> "Charlotte Maxeke"
-                    78 -> "Mahatma Gandhi"
+                    81 -> "Winnie Madikizela-Mandela. You are 81 years  old, which is the same age as Winnie Madikizela-Mandela."
+                    44 -> "David Webster. You are 44 years old, which is the same age as David Webster."
+                    91 -> "Helen Suzman. You are 91 years old, which is the same age as Helen Suzman."
+                    22 -> "Solomon Mahlangu. You are 22 years old, which is the same age as Solomon Mahlangu."
+                    30 -> "Steve Biko. You are 30 years old, which is the same age as Steve Biko."
+                    92 -> "Albertina Sisulu. You are 92 years old, which is the same age as Albertina Sisulu."
+                    69 -> "Lilian Masediba Ngoyi. You are 69 years old, which is the same age as Lilian Masediba Ngoyi."
+                    87 -> "Helen Joseph. You are 87 years old, which is the same age as Helen Joseph."
+                    68 -> "Charlotte Maxeke. You are 68 years old, which is the same age as Charlotte Maxeke."
+                    78 -> "Mahatma Gandhi. You are 78 years old, which is the same age as Mahatma Gandhi"
                     else -> null
                 }
 
-            val message = if (Historicalfigure != null) "The Historical figure's name is $Historicalfigure."
+            val message = if (Historicalfigure != null) "The Historical figure's name was $Historicalfigure."
                 else "No Historical figure found with the entered age."
                 resultTextView.text = message
 
 
             }else{
-                resultTextView.text = "Invalid input. Please enter a valid age between 20 and 10044."
+                resultTextView.text = "Invalid input. Please enter a valid age between 20 and 100."
             }
 
 
